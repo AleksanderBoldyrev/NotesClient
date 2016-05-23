c41972aa5dd64dfcccc59bdda27bd4c0207c65b1
AleksanderBoldyrev <aleksander.boldyrev@gmail.com>
2016-05-23 16:43:13 +0300

    Merge branch 'master' of https://github.com/AleksanderBoldyrev/NotesClient

--- app/src/test/java/notes/alex/notesclient/ExampleUnitTest.java
+++ app/src/test/java/notes/alex/notesclient/ExampleUnitTest.java
@@ -11,11 +11,6 @@ import static org.junit.Assert.*;
  */
 public class ExampleUnitTest {
     @Test
-    public void addition_isCorrect() throws Exception {
-        assertEquals(4, 2 + 2);
-    }
-
-    @Test
     public void testBuildTagList() throws Exception {
         RequestsParser tester = new RequestsParser();
         ArrayList<Tag> buff = new ArrayList<Tag>();
